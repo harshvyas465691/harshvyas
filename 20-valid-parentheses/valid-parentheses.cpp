832c5797ebe011b575b3a5db622e10ea42a05fd8
harshvyas465691 <drbhartisharma28@gmail.com>
2026-08-19 22:21:43 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (37.83%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -7,9 +7,9 @@ public:
         for(int i =0;i<s.size();i++){
             if(s[i]=='('|| s[i]=='{'|| s[i]=='[')st.push(s[i]);
             else if( i!=0 && !st.empty()){
-             if(st.top()=='('&& s[i]==')' && !st.empty()) st.pop();
-                        else if(st.top()=='{'&& s[i]=='}'&& !st.empty()) st.pop();
-            else if(st.top()=='['&& s[i]==']'&& !st.empty()) st.pop();else return false;}
+             if(st.top()=='('&& s[i]==')' ) st.pop();
+                        else if(st.top()=='{'&& s[i]=='}') st.pop();
+            else if(st.top()=='['&& s[i]==']') st.pop();else return false;}
             else return false;
 
         }
